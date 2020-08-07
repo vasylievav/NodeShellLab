2d615b3ad898db2ccc874abc5675d87f2b3a016a
Adam King <adking710@gmail.com>
2020-08-07 11:45:30 -0500

    Added pwd and ls commands to node shell

--- bash.js
+++ bash.js
@@ -1,7 +1,16 @@
+const pwd_func = require('./pwd.js')
+const ls_func = require('./ls.js')
+
+
 process.stdout.write('prompt>');
+
+pwd_func();
+ls_func();
 
 process.stdin.on('data',(data)=>{
 const cmd=data.toString().trim();
-process.stdout.write('You typed: ' + cmd);
-process.stdout.write('\nprompt > ');
+if((cmd !== 'pwd') && (cmd !== 'ls')) {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+    }
 });
---
+++ ls.js
@@ -0,0 +1,17 @@
+module.exports = function() {
+    const fs = require('fs');
+    process.stdin.on('data', (data) => {
+        const cmd = data.toString().trim();
+        if(cmd === 'ls') {
+            fs.readdir('./', 'utf8', (err, files) => {
+                if(err) {
+                    throw err
+                }
+                else {
+                    process.stdout.write(files.join('\n'));
+                    process.stdout.write('\nprompt >');
+                }
+            })
+        }
+    })
+}
