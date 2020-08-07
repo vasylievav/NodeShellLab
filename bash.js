05f6724ef65c886b56381185dbd21ee22c768f18
Valentyna Vasylieva <smithaddaf@gmail.com>
2020-08-07 14:15:25 -0500

    #4 task_2

--- bash.js
+++ bash.js
@@ -2,12 +2,16 @@ const pwd_func = require('./pwd.js')
 const ls_func = require('./ls.js')
 const cat = require('./cat.js')
 
+process.stdin.on('data', (data) => {
+    const input =data.toString().trim();
+    const command =input.split(' ')[0];
+    const filename =input.split(' ')[1]; )
 
 process.stdout.write('prompt>');
 
 pwd_func();
 ls_func();
-cat();
+cat(filename);
 
 // process.stdin.on('data',(data)=>{
 // const cmd=data.toString().trim();
